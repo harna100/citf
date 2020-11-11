15b96698b672ac8fd0ad82b1f50cefcb97bc67b9
Paul Harnack <harna100@mail.chapman.edu>
2020-11-11 00:19:22 -0800

    Fixes error where angular dev server wasn't being used
    Exposes angular test port
    
    Fixes nginx error if index.html didn't exist

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,11 +2,17 @@
 
 # Builds for dev or prod based on env variable
 if [ "${ENV}" = "PROD" ]; then
-    npm run build:prod
+    # keeps the command running forever so that nginx won't error out
+    # due to docker host not existing
+    npm run build:prod && tail -f /dev/null
 elif [ "${ENV}" = "TEST" ]; then
     npm run test
 elif [ "${ENV}" = "E2E" ]; then
     npm run e2e
 else
+    # remove any left over prod build files
+    # so that dev server will be used instead
+    # (may need to also be ran for test and e2e)
+    rm -r /client/dist/citf/*
     npm start
 fi
